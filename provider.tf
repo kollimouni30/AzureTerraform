19be0b7394a4d45ef27e8a41ab6dd0e89585e6d1
kollimouni30 <68189239+kollimouni30@users.noreply.github.com>
2021-06-04 00:57:18 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  version         = "2.0"
+  client_id       = "5a4ec545-94a8-4143-9c63-9914873036e5"
+  tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+  subscription_id = "7184eef3-f550-43a3-b584-8afb2e3812aa"
+  client_secret   = "Vq.6xr~CACJ541xP-lxRRAApl_667_9BGn"
+  features {}
+}
+
